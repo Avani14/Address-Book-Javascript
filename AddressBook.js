5dcf76c0f746d09c07cc663d37e05a517e13e1f2
Avani14 <trivediavani14@gmail.com>
2022-07-21 15:12:25 +0530

    Added the ability to add a contact

--- AddressBook.js
+++ AddressBook.js
@@ -9,6 +9,32 @@ class Contact{
         this.phone_number = phone_number
         this.email = email
     }
+    get FirstName() {
+        return this.first_name;
+    }
+     get LastName() {
+        return this.last_name
+    }
+     get Address() {
+        return this.address
+    }
+     get Zip() {
+        return this.zip
+    }
+     get City() {
+        return this.city
+    }
+     get State() {
+        return this.state
+    }
+     get PhoneNumber() {
+        return this.phone_number
+    }
+     get Email() {
+        return this.email
+    }
 }
 let person1 = new Contact("Avani","Trivedi","Places","Indore","MP",4534,987654332,"avani@gmail.com")
-console.log(person1);
+
+console.log(person1.Address);
+checkValidationForName(person1.FirstName)
